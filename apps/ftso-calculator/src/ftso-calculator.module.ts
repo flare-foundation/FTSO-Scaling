0060fd75a525040549b25855c4ed1ffe87acb519
Luka Avbreht <luka.avbreht@aflabs.si>
2024-01-11 15:57:15 +0100

    intial api client gen

--- apps/ftso-calculator/src/ftso-calculator.module.ts
+++ apps/ftso-calculator/src/ftso-calculator.module.ts
@@ -1,29 +1,10 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TLPEvents, TLPState, TLPTransaction } from "../../../libs/ftso-core/src/orm/entities";
+import configuration, { IConfig } from "./config/configuration";
 import { FtsoCalculatorController } from "./ftso-calculator.controller";
 import { FtsoCalculatorService } from "./ftso-calculator.service";
-import configuration, { IConfig } from "./config/configuration";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { FTSOParameters, FeedConfig } from "./config/FTSOParameters";
-import { PriceService } from "./price-feeds/price.service";
-import { IPriceProvider } from "../../../libs/ftso-core/src/IPriceFeed";
-import { RandomPriceFeed } from "./price-feeds/RandomPriceFeed";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { TLPTransaction, TLPEvents, TLPState } from "../../../libs/ftso-core/src/orm/entities";
-
-async function getPriceFeeds(feedConfigs: FeedConfig[]) {
-  return Promise.all(
-    feedConfigs.map(async (config, index) => {
-      let provider: IPriceProvider;
-      // TODO: Instantiate price provider dynamiocally
-      // if (config.providerImpl == "CcxtPriceFeed") {
-      //   provider = await CcxtPriceFeed.create(config);
-      // } else {
-        provider = new RandomPriceFeed(config.symbol, index);
-      // }
-      return provider;
-    })
-  );
-}
 
 const IMPORTS_ARRAY = [
   ConfigModule.forRoot({
@@ -53,19 +34,9 @@ const IMPORTS_ARRAY = [
   }),
 ];
 
-const priceServiceFactory = {
-  provide: "PRICE_SERVICE",
-  useFactory: async (config: ConfigService) => {
-    const params = config.get<FTSOParameters>("params");
-    const feeds = await getPriceFeeds(params.feeds);
-    return new PriceService(feeds);
-  },
-  inject: [ConfigService],
-};
-
 @Module({
   imports: IMPORTS_ARRAY,
   controllers: [FtsoCalculatorController],
-  providers: [FtsoCalculatorService, priceServiceFactory],
+  providers: [FtsoCalculatorService],
 })
 export class FtsoCalculatorModule {}
---
+++ apps/ftso-calculator/src/price-provider-api/provider-generator.ts
@@ -0,0 +1,88 @@
+const { generateApi } = require('swagger-typescript-api');
+const path = require("path");
+const fs = require("fs");
+
+// NOTE: its assumed that this scrypt is run from the root of the project
+
+const input = path.resolve(process.cwd(), './apps/ftso-calculator/src/price-provider-api/api-spec.json');
+const output = path.resolve(process.cwd(), './apps/ftso-calculator/src/price-provider-api/generated');
+
+console.log(`Generating API from ${input} to ${output}`);
+
+/* NOTE: all fields are optional expect one of `input`, `url`, `spec` */
+generateApi({
+  name: "provider-api.ts",
+  // set to `false` to prevent the tool from writing to disk
+  output: output,
+  input: input,
+  // templates: path.resolve(process.cwd(), './api-templates'),
+  httpClientType: "axios", // or "fetch"
+  defaultResponseAsSuccess: false,
+  generateClient: true,
+  generateRouteTypes: false,
+  generateResponses: true,
+  toJS: false,
+  extractRequestParams: false,
+  extractRequestBody: false,
+  extractEnums: true,
+  unwrapResponseData: false,
+  prettier: { // By default prettier config is load from your project
+    printWidth: 120,
+    tabWidth: 2,
+    trailingComma: "all",
+    parser: "typescript",
+  },
+  defaultResponseType: "void",
+  singleHttpClient: true,
+  cleanOutput: true,
+  enumNamesAsValues: false,
+  moduleNameFirstTag: true,
+  generateUnionEnums: false,
+  typePrefix: '',
+  typeSuffix: '',
+  enumKeyPrefix: '',
+  enumKeySuffix: '',
+  addReadonly: false,
+  sortTypes: false,
+  sortRouters: false,
+  extractingOptions: {
+    requestBodySuffix: ["Payload", "Body", "Input"],
+    requestParamsSuffix: ["Params"],
+    responseBodySuffix: ["Data", "Result", "Output"],
+    responseErrorSuffix: ["Error", "Fail", "Fails", "ErrorData", "HttpError", "BadResponse"],
+  },
+  /** allow to generate extra files based with this extra templates, see more below */
+  extraTemplates: [],
+  anotherArrayType: false,
+  fixInvalidTypeNamePrefix: "Type",
+  fixInvalidEnumKeyPrefix: "Value", 
+  codeGenConstructs: (constructs) => ({
+    ...constructs,
+    RecordType: (key, value) => `MyRecord<key, value>`
+  }),
+  primitiveTypeConstructs: (constructs) => ({
+      ...constructs,
+      string: {
+        'date-time': 'Date'
+      }
+  }),
+  hooks: {
+    onCreateComponent: (component) => {},
+    onCreateRequestParams: (rawType) => {},
+    onCreateRoute: (routeData) => {},
+    onCreateRouteName: (routeNameInfo, rawRouteInfo) => {},
+    onFormatRouteName: (routeInfo, templateRouteName) => {},
+    onFormatTypeName: (typeName, rawTypeName, schemaType) => {},
+    onInit: (configuration) => {},
+    onPreParseSchema: (originalSchema, typeName, schemaType) => {},
+    onParseSchema: (originalSchema, parsedSchema) => {},
+    onPrepareConfig: (currentConfiguration) => {},
+  }
+})
+  .then(({ files, configuration }) => {
+    files.forEach(({ content, name }) => {
+      fs.writeFile(path, content);
+    });
+  })
+  .catch(e => console.error(e))
+
