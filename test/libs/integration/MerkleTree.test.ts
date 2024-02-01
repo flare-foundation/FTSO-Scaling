8ae9ff1dc1d3572ae093acd62dd0b75b8f6d02a2
Luka Avbreht <luka.avbreht@aflabs.si>
2024-02-01 11:39:15 +0000

    using deterministic random

--- test/libs/integration/MerkleTree.test.ts
+++ test/libs/integration/MerkleTree.test.ts
@@ -2,15 +2,17 @@ import { expect } from "chai";
 import { MerkleTree } from "../../../libs/ftso-core/src/utils/MerkleTree";
 import { MerkleTreeStructs, TreeResult } from "../../../libs/ftso-core/src/utils/MerkleTreeStructs";
 import { MedianCalculationResult, RandomCalculationResult } from "../../../libs/ftso-core/src/voting-types";
-import e from "express";
+import Prando from "prando";
 
 describe("MerkleTree", () => {
+  const random = new Prando(42);
+
   it("Generate 100 feeds merkle tree and extract proofs", async () => {
     const VOTING_ROUND_ID = 10;
     const nodes: { object: TreeResult; hash: string; proof: string[] }[] = [];
     const randomResult: RandomCalculationResult = {
       votingRoundId: VOTING_ROUND_ID,
-      random: BigInt((Math.random() * 10000000000000000).toFixed(0)),
+      random: BigInt(random.nextInt(0, 10000000000000000)),
       isSecure: true,
     };
     nodes.push({
@@ -20,9 +22,9 @@ describe("MerkleTree", () => {
     });
     for (let i = 0; i < 100; i++) {
       const totalWeight = 10000000000000000;
-      const medianRes = Math.random() * 1000000000;
-      const delta1 = Math.random() * 1000;
-      const delta2 = Math.random() * 1000;
+      const medianRes = random.nextInt(0, 1000000000);
+      const delta1 = random.nextInt(0, 1000);
+      const delta2 = random.nextInt(0, 1000);
       const median: MedianCalculationResult = {
         votingRoundId: VOTING_ROUND_ID,
         feed: { name: i.toString(16).padEnd(16, "0"), decimals: 5 },
@@ -32,7 +34,7 @@ describe("MerkleTree", () => {
           finalMedianPrice: { value: Math.floor(medianRes), decimals: 5, isEmpty: false },
           quartile1Price: { value: Math.floor(medianRes - delta1 * 100000), decimals: 5, isEmpty: false },
           quartile3Price: { value: Math.floor(medianRes + delta2 * 100000), decimals: 5, isEmpty: false },
-          participatingWeight: BigInt((Math.random() * totalWeight).toFixed(0)),
+          participatingWeight: BigInt(random.nextInt(0, totalWeight)),
         },
         weights: [], // Used in calculation, not needed for merkle tree
         totalVotingWeight: BigInt(totalWeight),
@@ -53,6 +55,7 @@ describe("MerkleTree", () => {
     expect(merkleTree.root).to.be.not.undefined;
 
     expect(nodes[1].proof).to.be.not.undefined;
-    expect(nodes[1].proof.length).to.eq(Math.ceil(Math.log2(nodes.length)));
+    expect(nodes.length).to.eq(101);
+    expect(nodes[1].proof.length).to.eq(Math.floor(Math.log2(nodes.length)));
   });
 });
