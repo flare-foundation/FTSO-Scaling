48cb0d2eb926141043d859620c13609585046504
Andrius Dagys <andrius@flare.network>
2024-03-05 15:16:59 +0200

    Example provider: fix loading env variables

--- apps/example_provider/src/main.ts
+++ apps/example_provider/src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { NestFactory } from "@nestjs/core";
 import { RandomExampleProviderModule } from "./example-provider.module";
 import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from "@nestjs/swagger";
