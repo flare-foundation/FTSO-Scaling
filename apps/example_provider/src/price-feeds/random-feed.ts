198521fa850fd4396c439229addf62cfef7cc1f9
Andrius Dagys <andrius@flare.network>
2024-03-18 17:41:02 +0200

    Example provider: fix base value

--- apps/example_provider/src/price-feeds/random-feed.ts
+++ apps/example_provider/src/price-feeds/random-feed.ts
@@ -1,7 +1,7 @@
 import { FeedPriceData } from "../dto/provider-requests.dto";
 import { BaseDataFeed } from "./base-feed";
 
-const baseValue = 0.5;
+const baseValue = 0.05;
 
 export class RandomFeed implements BaseDataFeed {
   async getPrice(feed: string): Promise<FeedPriceData> {
